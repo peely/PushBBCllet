9221c14f3779883aaee47c4813466f1d2c05299c
peely <peelpeely@gmail.com>
2017-11-12 17:08:55 +0000

    files

---
+++ public/javascripts/page.js
@@ -0,0 +1,139 @@
+function registerUser(){
+	var Username = $('#Username').val();
+	var AccessToken = $('#AccessToken').val();
+
+	if(Username == "" || AccessToken == "")
+	{
+		//error
+	}
+	else{
+		//All ok
+		$.ajax({
+			url:'/users/register',
+			type:'POST',
+			data:{
+				username:Username,
+				accessToken: AccessToken
+			}
+		})
+		.done(registerCallSuccess)
+		.fail(ajaxFailure);
+	}
+
+	function registerCallSuccess(res, status){
+		console.log(status, res);
+
+		if(res.state == 0){
+			alert('user registered!');
+			listAllUsers();
+		}
+		else{
+			registerErrorHandle(res);
+		}
+	}
+
+	function registerErrorHandle(res){
+		alert('user already exists!');
+		switch(res.state){
+			case 1:
+				//Username already exists
+				$('#Username').toggleClass('alert-danger', true);
+				$('#Username').on('change', function(){ 
+					$(this).toggleClass('alert-danger', false);
+					$(this).off('change')
+				})
+			break;
+			default:
+				alert('An unknown error has occured');
+			break;
+		}
+	}
+}
+
+function listAllUsers()
+{
+	$.ajax({
+		url:'/users/list',
+		type:'GET'
+	})
+	.done(listAllUsersCallSuccess)
+	.fail(ajaxFailure);
+
+	function listAllUsersCallSuccess(res, status){
+		//STATUS
+		populateUsersTable(res);
+		populateUsersSelectMenu(res);
+	}
+
+	function populateUsersTable(res){
+		//Build the table body
+
+		var bodyHTML = "";
+		$(res).each(function(i, user){
+			bodyHTML += '<tr data-username="' + user.username + '">\n'
+			bodyHTML += '\t<td>' + user.username + '</td>\n'
+			bodyHTML += '\t<td>' + user.accessToken + '</td>\n'
+			bodyHTML += '\t<td>' + moment(user.creationTime).format('DD/MM/YY HH:mm:ss') + '</td>\n'
+			bodyHTML += '\t<td>' + user.numOfNotificationsPushed + '</td>\n'
+			bodyHTML += '</tr>\n';
+		});
+
+		$('#allRegisteredUsersTable tbody tr').off('click');
+		$('#allRegisteredUsersTable tbody').html(bodyHTML);
+		$('#allRegisteredUsersTable tbody tr').on('click', function(e){
+			var selectedUserName = $(this).data('username');
+			$('#sendNotificationTo').val(selectedUserName);
+		});
+	}
+
+	function populateUsersSelectMenu(res){
+		var selectHTML = "";
+		$(res).each(function(i, user){
+			selectHTML += '<option value="' + user.username + '"> ' + user.username + '</option>\n'
+		});
+		$('#sendNotificationTo').html(selectHTML);
+
+	}
+}
+
+function sendNotification()
+{
+	var notificationTitle = $('#notificationTitle').val();
+	var notificationMessage = $('#notificationMessage').val();
+	$.ajax({
+		url:'/users/push',
+		type:'POST',
+		data:{
+			title: notificationTitle,
+			message: notificationMessage,
+			username: $('#sendNotificationTo').val()
+		}
+	})
+	.done(sendNotificationCallSuccess)
+	.fail(ajaxFailure);
+
+	function sendNotificationCallSuccess(res, status){
+		if(res.status == 0){
+			console.log(res.pushbulletResponse);
+		}
+		else{
+			sendNotificationErrorHandle(res);
+		}
+	}
+
+	function sendNotificationErrorHandle(res){
+		switch(res.status){
+			case 1:
+			alert('User does not exist!');
+			break;
+			default:
+			alert('An unknown error has occured');
+			break;
+		}
+	}
+}
+
+
+function ajaxFailure(reqObj, status){
+	console.log(status, reqObj);
+}
---
+++ routes/users.js
@@ -0,0 +1,123 @@
+var express = require('express');
+var router = express.Router();
+
+router.post('/register', function(req, res, next) {
+	//console.log('register');
+
+	//Get new users details
+	var uname = req.body['username'];
+	var AT = req.body['accessToken'];
+
+	var responseObj = {}
+	responseObj.state = 0;
+
+	if(uname && AT){
+		//Get Current users
+		var allUsers = req.app.get('allUsers');
+
+		//Have we seen this username already?
+		var existingUser = allUsers.filter(x => x.username == uname)[0];
+		if(existingUser){
+			 //error - username already exists
+			 responseObj.state = 1;
+		}
+		else
+		{//user does not exist, so create and add to array
+
+			existingUser = {
+				username:uname, 
+				accessToken:AT,
+				creationTime:new Date(),
+				numOfNotificationsPushed:0
+			}
+
+			responseObj.newUser = existingUser;
+
+			allUsers.push(existingUser);
+		}
+	}
+
+	res.send(responseObj);
+});
+
+
+
+router.get('/list', function(req, res, next) {
+  //Get Current users
+  var allUsers = req.app.get('allUsers');
+  res.send(allUsers);
+});
+
+
+
+router.post('/push', function(req, res, next) {
+  	//console.log('push');
+
+	var title = req.body["title"];
+	var message = req.body['message'];
+	var uname = req.body['username'];
+
+	//console.log(message, uname);
+
+	//Get Current users
+	var allUsers = req.app.get('allUsers');
+
+	//Have we seen this username already?
+	var existingUser = allUsers.filter(x => x.username == uname)[0];
+
+	var responseObj = {};
+	responseObj.status = 0;
+
+	if(existingUser)
+	{
+  		var querystring = require('querystring');
+		var data = querystring.stringify({
+			'active':'true',
+			'type':'note',
+			'title':title,
+			'body': message,
+			'direction':'self',
+			'dismissed':'false'
+	    });
+
+		var options = {
+		    host: 'api.pushbullet.com',
+		    port: 443,
+		    path: '/v2/pushes',
+		    method: 'POST',
+		    headers: {
+		        'Content-Type': 'application/x-www-form-urlencoded',
+		        'Content-Length': Buffer.byteLength(data),
+		        'Access-Token': existingUser.accessToken
+	    	}
+		};
+
+		var https = require('https');
+		var pushbulletRequest = https.request(options, function(pushbulletRes) 
+		{
+		    //var chunkCount = 0
+		    var result = '';
+		    pushbulletRes.on('data', function (chunk) {
+		    	//chunkCount++;
+		        //console.log('Got a Chunk: ' + chunkCount);
+		        result += chunk;
+		    });
+
+		    pushbulletRes.on('end', function() {
+	            var obj = JSON.parse(result);
+	            //console.log(pushbulletRes.statusCode)//, obj);
+	            responseObj.pushbulletResponse = obj;
+	            res.send(responseObj);
+	        });
+		});
+
+		pushbulletRequest.write(data);
+		pushbulletRequest.end();
+	}
+	else{
+		responseObj.status = 1;
+		res.send(responseObj);
+	}
+});
+
+module.exports = router;
